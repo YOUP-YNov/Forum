5ca3980d44aa37594bf725d45f05ea7632506857
naxxan60 <morganlebihan.89@gmail.com>
2014-10-31 19:17:39 +0100

    ToBusiness List et Forum Business

--- Forum/Business/ConvertBusiness.cs
+++ Forum/Business/ConvertBusiness.cs
@@ -10,6 +10,7 @@ namespace Forum.Business
 {
     public static class ConvertBusiness
     {
+        //TO BUSINESS
         public static ForumB ToBusiness(ForumD forum)
         {
             Mapper.CreateMap<ForumD, ForumB>();
@@ -22,8 +23,6 @@ namespace Forum.Business
             return Mapper.Map<TopicD, TopicB>(topic);
         }
 
-       
-
         public static CategorieB ToBusiness(CategorieD categoy)
         {
             Mapper.CreateMap<CategorieD, CategorieB>();
@@ -34,6 +33,50 @@ namespace Forum.Business
         {
             Mapper.CreateMap<MessageD, MessageB>();
             return Mapper.Map<MessageD, MessageB>(message);
+        }
+
+        //TO BUSINESS LIST
+        public static List<ForumB> ToBusiness(List<ForumD> listforumd)
+        {
+            List<ForumB> listforumb = new List<ForumB>();
+            Mapper.CreateMap<ForumD, ForumB>();
+
+            foreach ( var forumb in listforumd) {
+                listforumb.Add(Mapper.Map<ForumD, ForumB>(forumb));
+            }
+            return listforumb;
+        }
+
+        public static TopicB ToBusiness(TopicD topic)
+        {
+            Mapper.CreateMap<TopicD, TopicB>();
+            return Mapper.Map<TopicD, TopicB>(topic);
+        }
+
+
+        //TO DAL
+        public static ForumD ToDAL(ForumB forum)
+        {
+            Mapper.CreateMap<ForumB, ForumD>();
+            return Mapper.Map<ForumB, ForumD>(forum);
+        }
+
+        public static TopicD ToDAL(TopicB topic)
+        {
+            Mapper.CreateMap<TopicB, TopicD>();
+            return Mapper.Map<TopicB, TopicD>(topic);
+        }
+
+        public static CategorieD ToDAL(CategorieB categoy)
+        {
+            Mapper.CreateMap<CategorieB, CategorieD>();
+            return Mapper.Map<CategorieB, CategorieD>(categoy);
+        }
+
+        public static MessageD ToDAL(MessageB message)
+        {
+            Mapper.CreateMap<MessageB, MessageD>();
+            return Mapper.Map<MessageB, MessageD>(message);
         }
     }
 }
