6767aaf77c59c4a033329c5db30b14af6698eb57
Fergal-N <Fergal86@hotmail.fr>
2014-10-31 15:50:54 +0100

    synchroGlobale

--- Forum/Controllers/FakeMessageController.cs
+++ Forum/Controllers/FakeMessageController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Forum.DAL.Data;
+using Forum.DAL;
 
 namespace Forum.Controllers
 {
@@ -11,7 +13,10 @@ namespace Forum.Controllers
         // GET: FakeMessage
         public ActionResult Index()
         {
-            return View();
+            List<MessageD> liste = new List<MessageD>();
+            MessageDAL mes = new MessageDAL();
+            liste = mes.GetListTopicMessage(1);
+            return View(liste);
         }
 
         // GET: FakeMessage/Details/5
