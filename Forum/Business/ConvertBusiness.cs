33d3b395f81c5e3917878c8e8a3e26b90b5cb1eb
naxxan60 <morganlebihan.89@gmail.com>
2014-10-31 17:02:02 +0100

    AutoMapper - 1er  convert classe forum

---
+++ Forum/Business/ConvertBusiness.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Forum.Business.Data;
+using Forum.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forum.Business
+{
+    public static class ConvertBusiness
+    {
+        public static ForumB ToBusiness(ForumD forum)
+        {
+            Mapper.CreateMap<ForumD, ForumB>();
+            return Mapper.Map<ForumD, ForumB>(forum);
+        }
+    }
+}
