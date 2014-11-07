7ac914914a62d37d83a1a05e65be3fc89ca77d24
Charles-Junior <e.charles-junior@hotmail.com>
2014-11-07 18:51:32 +0100

    [Message][DSF04][001][002][003][005][006] Implementation du MessageController

--- Forum/Models/ConvertModel.cs
+++ Forum/Models/ConvertModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,5 +9,31 @@ namespace Forum.Models
     public class ConvertModel
     {
 
+
+        internal static List<MessageModel> ToModel(List<Business.Data.MessageB> list)
+        {
+            List<MessageModel> listmessage = new List<MessageModel>();
+            Mapper.CreateMap<Business.Data.MessageB, MessageModel>();
+
+            foreach (var messagemodel in list)
+            {
+                listmessage.Add(Mapper.Map<Business.Data.MessageB, MessageModel>(messagemodel));
+            }
+            return listmessage ;
+        }
+
+        internal static MessageModel ToModel(Business.Data.MessageB messageB)
+        {
+            Mapper.CreateMap<Business.Data.MessageB, MessageModel>();
+            return Mapper.Map<Business.Data.MessageB, MessageModel>(messageB);
+        }
+
+
+
+        internal static Business.Data.MessageB ToBusiness(MessageModel Message)
+        {
+            Mapper.CreateMap<MessageModel, Business.Data.MessageB>();
+            return Mapper.Map<MessageModel, Business.Data.MessageB>(Message);
+        }
     }
 }
