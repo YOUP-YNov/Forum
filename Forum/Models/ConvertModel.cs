b325b8dd7599cf922345a9a1172f081af8f3be4f
aurelienMerzeau17 <aurelien.merzeau@gmail.com>
2014-11-07 17:35:18 +0100

    Correction Convertion de listes avec automapper

--- Forum/Models/ConvertModel.cs
+++ Forum/Models/ConvertModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoMapper;
+using Forum.Business.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +9,16 @@ namespace Forum.Models
 {
     public class ConvertModel
     {
+        internal static List<ForumModel> ToModel(List<Business.Data.ForumB> list)
+        {
+            List<ForumModel> listforumM = new List<ForumModel>();
+            Mapper.CreateMap<ForumB, ForumModel>();
 
+            foreach (var forumb in list)
+            {
+                listforumM.Add(Mapper.Map<ForumB, ForumModel>(forumb));
+            }
+            return listforumM;
+        }
     }
 }
