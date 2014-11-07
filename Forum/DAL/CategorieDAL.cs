c980bfcefeb2fd992d822569742cf11ab1dcb685
Thibaud Cutullic <tcutullic@gmail.com>
2014-11-07 11:10:08 +0100

    PS CatégorieDAL Insert

--- Forum/DAL/CategorieDAL.cs
+++ Forum/DAL/CategorieDAL.cs
@@ -1,16 +1,52 @@
 ﻿using Forum.DAL.Data;
 using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
 
 namespace Forum.DAL
-{
-    public class CategorieDAL
+{   
+    //public class CategorieDAL
+    public partial class CategorieDAL
     {
-        string connexionstring = "data source=avip9np4yy.database.windows.net,1433;initial catalog=YoupDEV;persist security info=True;user id=youpDEV;password=youpD3VASP*;MultipleActiveResultSets=True;App=EntityFramework";
+        [SqlProcedure()]
+        //string connexionstring = "data source=avip9np4yy.database.windows.net,1433;initial catalog=YoupDEV;persist security info=True;user id=youpDEV;password=youpD3VASP*;MultipleActiveResultSets=True;App=EntityFramework";
         SqlConnection myConnection;
+
+        public static void CreateCategorie(SqlInt32 sujet_id, SqlInt32 forum_id, SqlString nom)
+        {
+            using (SqlConnection conn = new SqlConnection("data source=avip9np4yy.database.windows.net,1433;initial catalog=YoupDEV;persist security info=True;user id=youpDEV;password=youpD3VASP*;MultipleActiveResultSets=True;App=EntityFramework"))
+            {
+                SqlCommand CreateCategorieCommand = new SqlCommand();
+                SqlParameter sujetIdParam = new SqlParameter("@Sujet_id", SqlDbType.BigInt);
+                SqlParameter forumIdParam = new SqlParameter("@Forum_id", SqlDbType.BigInt);
+                SqlParameter nomParam = new SqlParameter("@nom", SqlDbType.NVarChar);
+
+                sujetIdParam.Value = sujet_id;
+                forumIdParam.Value = forum_id;
+                nomParam.Value = nom;
+
+                CreateCategorieCommand.Parameters.Add(sujetIdParam);
+                CreateCategorieCommand.Parameters.Add(forumIdParam);
+                CreateCategorieCommand.Parameters.Add(nomParam);
+
+                CreateCategorieCommand.CommandText =
+                    "INSERT [dbo].[FOR_Sujet] (Sujet_id, Forum_id, Nom)" +
+                    " VALUES(@Sujet_id, @Forum_id, Nom)";
+
+                CreateCategorieCommand.Connection = conn;
+
+                conn.Open();
+                CreateCategorieCommand.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        /*
         public CategorieDAL()
         {
             myConnection = new SqlConnection(connexionstring);
@@ -33,7 +69,7 @@ namespace Forum.DAL
                 command.ExecuteNonQuery();
             }
         }
-        
+        */
         public void EditCategorie(CategorieD cat)
         {
             using (SqlCommand command = new SqlCommand())
